0a04a4855300da765005f412820076c45ee7ee87
Peter de Croos <cultofmetatron@aumlogic.com>
2016-04-23 19:25:53 -0700

    completed pearson

--- ch_02_making_reccomendations/src/ch_02_making_reccomendations/core.clj
+++ ch_02_making_reccomendations/src/ch_02_making_reccomendations/core.clj
@@ -80,7 +80,7 @@
   (apply clojure.set/intersection
     (map (fn [item]  (set (keys item))) items)))
 
-; note: the original code did not perform the sqrt. this did nor make sense
+; note: the original code did not perform the sqrt. this did not make sense
 ; as the preceeding code hinted at needing it so I put it in.
 (defn sim_distance
   "calculates the similarity distance between two records"
@@ -92,3 +92,51 @@
                               (shared_keys (get prefs person-1) (get prefs person-2)))))
     ]
     (/ 1 (+ 1 distance))))
+
+
+(defn extract-values
+  "returns the values for the keys as a sequence"
+  [prefs person keys]
+  (let [
+        ratings (get prefs person)
+        values (map #(get ratings %1) (keys ratings))
+        ]
+    values))
+
+(defn pull-values
+  "pulls the values out of the person from a set"
+  [keyset prefs person]
+  (map
+    (fn [obj]
+      (get (get prefs person) obj))
+    keyset))
+
+(defn sum  [coll] (reduce #(+ %1 %2) coll))
+
+(defn square-sum [values]
+  (sum
+    (map #(* %1 %1)  values)))
+
+(defn sim-pearson
+  "calculates the pearson score between two reveiwers"
+  [prefs person-1 person-2]
+  (let [
+        shared (shared_keys (get prefs person-1) (get prefs person-2))
+        p1-values (pull-values shared prefs person-1)
+        p2-values (pull-values shared prefs person-2)
+        p1-sum (sum p1-values)
+        p2-sum (sum p2-values)
+        p1-square-sum (square-sum p1-values)
+        p2-square-sum (square-sum p2-values)
+        p-sum (sum (map
+                     (fn [[arg1 arg2]] (* arg1 arg2))
+                     (map vector p1-values p2-values)) )
+        num (- p-sum (/ (* p1-sum p2-sum) (count shared)))
+        den (Math/sqrt (*
+                         (- p1-square-sum (/ (Math/pow p1-sum 2) (count shared) ))
+                         (- p2-square-sum (/ (Math/pow p2-sum 2) (count shared) ))
+                         ))
+      ] (cond
+                 (<= (count shared) 0)  0
+                 (= den 0) 0
+                 :else (/ num den))))
